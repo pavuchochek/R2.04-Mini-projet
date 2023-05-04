516c81088ec8b42bb07068bae8fe1ef29f7bc551
Gamingdy <christgik12@gmail.com>
2023-05-04 16:49:32 +0200

    feat: ajout de checkalpha dans utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,9 @@
 #ifndef UTILS
 #define UTILS
 
+#include <stdbool.h>
 
-
+bool checkalpha (char message[]);
 char * user_input (int *input_size);
 int remove_accent(char dest[], const char src[]);
 int Ecriture(char org[],char str[]);
