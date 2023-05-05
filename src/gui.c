e6d6facc1adf9e273fe4d3658928d31e2be593c5
mrmoi31 <dodo.leva.dl@gmail.com>
2023-05-05 12:47:33 +0200

    feat: affichage gui

--- src/gui.c
+++ src/gui.c
@@ -27,7 +27,8 @@ char* getText()
         return NULL;
     }
 
-    window = SDL_CreateWindow("Saisie de texte", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Chiffrement de César", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN); 
+                                                                                                     //(bug) SDL_WINDOW_RESIZABLE
 
     if (window == NULL)
     {
@@ -41,15 +42,66 @@ char* getText()
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    TTF_Font *font = TTF_OpenFont("font/Lato-Black.ttf", 24);
+    TTF_Font *font = TTF_OpenFont("font/Lato-Black.ttf", 36);
     if (font == NULL){
         fprintf(stderr, "Erreur de font %s \n", TTF_GetError());
     }
 
     SDL_Color color = { 0, 0, 0, 255 };
 
-    printf("Veuillez saisir un texte à coder svp");
+    printf("Veuillez saisir un texte à coder svp\n");
 
+    // AFFICHAGE MESSAGE CACHE
+    SDL_Surface *surface4 = TTF_RenderText_Blended(font, "votre message : ", color);
+    SDL_Texture *texture4 = SDL_CreateTextureFromSurface(renderer, surface4);
+    
+    int textWidth, textHeight;
+    SDL_QueryTexture(texture4, NULL, NULL, &textWidth, &textHeight);
+    
+    SDL_Rect textRect4 = { (640 - textWidth) / 2, (480 - textHeight) / 2-50, textWidth, textHeight };
+    
+    SDL_RenderCopy(renderer, texture4, NULL, &textRect4);
+    
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(texture4);
+    SDL_FreeSurface(surface4);
+    //------------------------------------------
+
+    // AFFICHAGE MESSAGE CODE    
+    SDL_Surface *surface2 = TTF_RenderText_Blended(font, "votre message code : ", color);
+    SDL_Texture *texture2 = SDL_CreateTextureFromSurface(renderer, surface2);
+    
+    SDL_QueryTexture(texture2, NULL, NULL, &textWidth, &textHeight);
+    
+    SDL_Rect textRect2 = { (640 - textWidth) / 2, (480 - textHeight) / 2+50, textWidth, textHeight };
+    
+    SDL_RenderCopy(renderer, texture2, NULL, &textRect2);
+    
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(texture2);
+    SDL_FreeSurface(surface2);
+    //----------------------------------------------------
+
+    // AFFICHAGE CLE
+    TTF_Font *font2 = TTF_OpenFont("font/Lato-BlackItalic.ttf", 24);
+
+    SDL_Surface *surface3 = TTF_RenderText_Blended(font2, "Cle : 10", color);
+    SDL_Texture *texture3 = SDL_CreateTextureFromSurface(renderer, surface3);
+    
+    SDL_QueryTexture(texture3, NULL, NULL, &textWidth, &textHeight);
+    
+    SDL_Rect textRect3 = { (640 - textWidth) / 2-230, (480 - textHeight) / 2-46, textWidth, textHeight };
+    
+    SDL_RenderCopy(renderer, texture3, NULL, &textRect3);
+    
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(texture3);
+    SDL_FreeSurface(surface3);
+    //--------------------------------------------
+    
     while (!done)
     {
         SDL_Event event;
@@ -64,11 +116,15 @@ char* getText()
                 strcat(text, event.text.text);
                 textLength++;
                 for (int i; i<textLength; i++){
-                    stars[i]="a";
+                    stars[i]='*';
                     printf("Taille de stars actuelle : %d\n", i);
                 }
 
-                SDL_Surface *surface = TTF_RenderText_Blended(font, stars, color);
+                TTF_Font *font = TTF_OpenFont("font/Lato-Black.ttf", 36);
+
+                SDL_Surface *surface = TTF_RenderText_Blended(font, stars, color); 
+                //"text" à la place de "stars" pour afficher le message (bug)
+                
                 SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
                 
                 int textWidth, textHeight;
@@ -112,6 +168,7 @@ char* getText()
 int main(int argc, char *argv[])
 {
     char* txt = getText();
-    //printf("Texte renvoyé : %s\n", txt);
+    //ChiffrementCesar(txt,10);
+    printf("Texte renvoyé : %s\n", txt);
     return EXIT_SUCCESS;
 }
